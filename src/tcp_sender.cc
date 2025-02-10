65dd2add0c1383e4f3439ab4d5c98967ab23c0a0
oburay <oburay13@gmail.com>
2025-02-10 07:52:37 +0000

    implementation for TCPSender::push

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -24,7 +24,40 @@ uint64_t TCPSender::consecutive_retransmissions() const
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  
+  (void)transmit;
+  TCPSenderMessage segment {};
+
+  if ( ( ( window_size > sequence_numbers_in_flight() ) && ( reader().bytes_buffered() > 0 ) )
+       || next_stream_index == 0
+       || ( reader().is_finished() && !stream_ended && ( window_size > sequence_numbers_in_flight() ) ) ) {
+
+    uint64_t effective_windows_size = window_size - sequence_numbers_in_flight();
+    do {
+      segment.SYN = ( next_stream_index == 0 ) ? true : false;
+      segment.seqno = isn_ + next_stream_index;
+      std::string data = static_cast<std::string>( reader().peek() );
+      data.resize( min( data.size(), min( effective_windows_size, TCPConfig::MAX_PAYLOAD_SIZE ) ) );
+
+      if ( segment.SYN && ( reader().bytes_buffered() > 0 ) && ( effective_windows_size <= data.size() ) ) {
+        data.resize( data.size() - 1 );
+      }
+      segment.payload = data;
+      segment.RST = ( reader().has_error() ? true : false );
+      reader().pop( data.size() );
+      segment.FIN
+        = ( reader().is_finished() && ( ( segment.SYN + segment.payload.size() + 1 ) <= effective_windows_size )
+              ? true
+              : false );
+      effective_windows_size -= segment.sequence_length();
+      oustandings.push_back( segment );
+      stream_ended = segment.FIN ? true : false;
+
+      transmit( segment );
+      next_stream_index += segment.sequence_length();
+    } while ( ( segment.payload.size() == TCPConfig::MAX_PAYLOAD_SIZE ) && ( reader().bytes_buffered() > 0 ) );
+  }
+
+  return;
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
